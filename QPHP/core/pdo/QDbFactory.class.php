8ff2e43787e0ef3ffd372919ab58e7137009f8dc
1211884772 <1211884772@qq.com>
2022-05-29 22:28:37 +0800

    mysql和oracle实现limit分页功能

--- QPHP/core/pdo/QDbFactory.class.php
+++ QPHP/core/pdo/QDbFactory.class.php
@@ -3,14 +3,22 @@
 
 class QDbFactory
 {
-
-public function getDb($dbType='mysql')
+static public $qdb=null;
+static public function getDb($dbType='mysql')
 {
     if(!empty($dbType)) {
         if($dbType==='mysql'){
-            return new QDbMysql();
+            if(self::$qdb instanceof QDbMysql){
+                return self::$qdb;
+            }else{
+                return new QDbMysql();
+            }
         }elseif ($dbType==='oracle') {
-            return new QDbOracle();
+            if(self::$qdb instanceof QDbOracle){
+                return self::$qdb;
+            }else{
+                return new QDbOracle();
+            }
         }
     }
     return null;
