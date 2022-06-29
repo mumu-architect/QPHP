3c7bd1f0d8503f0490885a41885dd14450ecb393
1211884772 <1211884772@qq.com>
2022-06-30 04:08:37 +0800

    QPHP核心框架，model新增IModelFactory抽象工厂类，优化模型类

--- QPHP/core/model/ModelFactory.class.php
+++ QPHP/core/model/ModelFactory.class.php
@@ -6,22 +6,17 @@ class ModelFactory implements IModelFactory
     public function createModel($dbType,$table,$key){
         if(!empty($dbType)) {
             try{
-                $db_type = ucfirst($dbType);
-                $model_class = $db_type."M";
-                if(class_exists($model_class)){
-                    return new $model_class($table,$key);
+                if($dbType==='mysql'){
+                    $model=new MysqlM($table,$key);
+                }elseif ($this->dbType==='oracle') {
+                    $model=new OracleM($table,$key);
+                }else{
+                    throw new Exception("Model type error");
                 }
             }catch (Exception $e){
                 throw $e;
             }
-            /* if($this->dbType==='mysql'){
-                 $this->interface_model=new MysqlModel($this->table,$this->key);
-             }elseif ($this->dbType==='oracle') {
-                 $this->interface_model=new OracleModel($this->table,$this->key);
-             }else{
-                 throw new Exception("Model type error");
-             }*/
-            return null;
+            return $model;
         }else{
             throw new Exception("The model type is empty");
         }
